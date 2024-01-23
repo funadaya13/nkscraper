586daab9f27b983a1ae1f54ceb5f348cb2446f36
funadaya13 <funadaya13@gmail.com>
2024-01-23 21:51:44 +0900

    #8 コース種別改良.

--- nkscraper/common/netkeiba_corse_type.py
+++ nkscraper/common/netkeiba_corse_type.py
@@ -2,6 +2,8 @@
 """ netkeiba コース種別モジュール
 """
 
+from __future__ import annotations
+
 # built-in
 from enum import Enum
 
@@ -23,3 +25,14 @@ class NetkeibaCorseType(Enum):
             return 'ダ'
         elif self == NetkeibaCorseType.JUMP:
             return '障' 
+
+    @staticmethod
+    def from_course_type(course_type: str) -> NetkeibaCorseType:
+        """ コース種別から netkeiba コース種別を作成する.
+        """
+        if course_type == '芝':
+            return NetkeibaCorseType.SHIBA
+        elif course_type == 'ダ':
+            return NetkeibaCorseType.DIRT
+        elif course_type == '障':
+            return NetkeibaCorseType.JUMP
